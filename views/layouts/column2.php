28048232d7d7d6f2a092c3fb089a85e6283ca2ad
Alexander Schilling <dignityinside@gmail.com>
2018-11-17 13:20:13 +0100

    Small fixes

--- views/layouts/column2.php
+++ views/layouts/column2.php
@@ -7,20 +7,17 @@
                     Наше сообщество
                 </div>
                 <div class="widget-content">
-                    <div class="widget-content-social">
-                        <i class="fa fa-youtube"></i> <a href="https://youtube.com/dignityinside" target="_blank" rel="nofollow noopener">Канал на YouTube</a>
-                    </div>
-                    <div class="widget-content-social">
-                        <i class="fa fa-vk"></i> <a href="https://vk.com/phpland" target="_blank" rel="nofollow noopener">Группа ВК</a>
-                    </div>
-                    <div class="widget-content-social">
-                        <i class="fa fa-telegram"></i> <a href="https://t.me/phpland" target="_blank" rel="nofollow noopener">Канал в Telegram</a> | <a href="https://t.me/phpland_chat" target="_blank" rel="nofollow noopener">Чат</a>
-                    </div>
-                    <div class="widget-content-social">
-                        <i class="fa fa-twitter"></i> <a href="https://twitter.com/phplandorg" target="_blank" rel="nofollow noopener">Микроблог в Twitter</a>
-                    </div>
-                    <div class="widget-content-social">
-                        <i class="fa fa-github"></i> <a href="https://github.com/phpland" target="_blank" rel="nofollow noopener">Исходники на Github</a>
+                    <div class="widget-content__social-icons">
+                        <a href="https://youtube.com/dignityinside" target="_blank" rel="nofollow" title="Канал на YouTube"><i class="fa fa-youtube"></i></a>
+                        <a href="https://t.me/phpland" target="_blank" rel="nofollow" title="Telegram канал"><i class="fa fa-telegram"></i></a>
+                        <a href="https://t.me/phpland_chat" target="_blank" rel="nofollow" title="Telegram чат"><i class="fa fa-users"></i></a>
+                        <a href="https://phpland.slack.com" target="_blank" rel="nofollow" title="Slack"><i class="fa fa-slack"></i></a>
+                        <a href="https://discord.gg/PPtFTZh" target="_blank" rel="nofollow" title="Slack"><i class="fab fa-discord"></i></a>
+                        <a href="https://vk.com/phpland" target="_blank" rel="nofollow" title="Группа ВК"><i class="fa fa-vk"></i></a>
+                        <a href="https://twitter.com/phplandorg" target="_blank" rel="nofollow" title="Микроблог в Twitter"><i class="fa fa-twitter"></i></a>
+                        <a href="https://mastodon.social/@phplandorg" rel="me nofollow" target="_blank"><i class="fab fa-mastodon"></i></a>
+                        <a href="https://github.com/phpland" target="_blank" rel="nofollow" title="Исходный код на Github"><i class="fa fa-github"></i></a>
+                        <a href="https://instagram.com/phpland" target="_blank" rel="nofollow" title="Instagram"><i class="fab fa-instagram"></i></a>
                     </div>
                 </div>
             </div>
@@ -31,14 +28,6 @@
                 <div class="widget-content">
                     <p>Я с удовольствием и благодарностью приму маленькие пожертвования.</p>
                     <p><?= \yii\helpers\Html::a('Подробнее »', '/donate')?></p>
-                </div>
-            </div>
-            <div class="widget">
-                <div class="widget-title">
-                    Рекомендую
-                </div>
-                <div class="widget-content">
-                    <?= yii\helpers\Html::a(yii\helpers\Html::img('img/php-the-right-way.png'), 'http://www.phptherightway.com', ['rel'=>'nofollow', 'target'=>'_blank']) ?>
                 </div>
             </div>
         </div>
