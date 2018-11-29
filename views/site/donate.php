55dc4683f315dfe030ecc2522d6e21bc43e362f0
Alexander Schilling <dignityinside@gmail.com>
2018-11-29 23:29:12 +0100

    Small fixes

--- views/site/donate.php
+++ views/site/donate.php
@@ -10,16 +10,20 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 
 <div class="site-donate">
-    <h1><?= Html::encode($this->title) ?></h1>
-    <p>Я с удовольствием и благодарностью приму маленькие пожертвования (donate).<br>Данные моих счетов здесь всегда в актуальном виде, можно не уточнять.</p>
 
-    <h3>Проще всего поддержать проект с помощью:</h3>
+    <h1><i class="fas fa-donate"></i> <?= Html::encode($this->title) ?></h1>
+
+    <p>Мы с удовольствием и благодарностью примем любое пожертвования (donate). Все пожертвования идут на нужды сообщества (оплату сервера, домейна, разработку нового функционала, запись новых видео уроков и т.д).
+        Все реквизиты всегда в актуальном виде, можно не уточнять.</p>
+
+    <h3>Реквизиты</h3>
 
     <?php
 
         $payments = [
             ['provider' => 'Яндекс-Деньги', 'account' => '<a href="https://money.yandex.ru/to/41001609245268">41001609245268</a>'],
             ['provider' => 'Webmoney', 'account' => 'R321159265992'],
+            ['provider' => 'PayPal', 'account' => '<a href="https://www.paypal.me/dignityinside">dignityinside</a>'],
             ['provider' => 'Bitshares', 'account' => '<a href="https://market.rudex.org/#/transfer">alexander-schilling</a>'],
         ];
 
@@ -42,46 +46,6 @@ $this->params['breadcrumbs'][] = $this->title;
         </tbody>
     </table>
 
-    <p class="hint-block">Не забудьте указать в комментарии слово phpland и ваше имя или псевдоним, которое будет отображаться ниже (например так: phpland, Александр)</p>
-
-    <h3>Доска почета (список донэйшнов от 100 ₽). Спасибо за поддержку!</h3>
-
-    <?php
-
-        // list of donations
-        $donations = [
-            ['name' => 'Валерий', 'value' => '1500'],
-            ['name' => 'Валерий', 'value' => '1000'],
-            ['name' => 'Валерий', 'value' => '1000'],
-            ['name' => 'Аноним', 'value' => '800'],
-            ['name' => 'Аноним', 'value' => '500'],
-            ['name' => 'Аноним', 'value' => '400'],
-            ['name' => 'Аноним', 'value' => '400'],
-            ['name' => 'Аноним', 'value' => '300'],
-            ['name' => 'Аноним', 'value' => '120'],
-            ['name' => 'Валерий', 'value' => '150'],
-        ];
-
-    ?>
-
-    <table class="table table-hover">
-        <thead>
-        <tr >
-            <th>№</th>
-            <th>Сумма</th>
-            <th>Имя/Псевдоним</th>
-        </tr>
-        </thead>
-        <tbody>
-            <?php $i = 1; ?>
-            <?php foreach ($donations as $donation) : ?>
-                <tr>
-                    <td><?= $i++; ?></td>
-                    <td><?= $donation['value']; ?> ₽</td>
-                    <td><i class="fa fa-user"></i> <?= $donation['name']; ?></td>
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+    <p>Спасибо за поддержку!</p>
 
 </div>
